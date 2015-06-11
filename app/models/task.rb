cd5606a2666e939bb0c00b67b22fd511dc6ecd61
emilianov <Emiliano@Emilianos-MacBook-Pro.local>
2015-06-11 09:12:01 -0500

    Adds comments

--- app/models/task.rb
+++ app/models/task.rb
@@ -12,29 +12,31 @@ class Task < ActiveRecord::Base
 
   def self.today
     tasks = all # TODO: incomplete tasks only
-    tasks_by_day = Array.new
+    tasks_by_day = Array.new # Holds the day (which holds tasks)
 
+    # Run the loop to assign each task to its day
     while tasks.count > 0 do
       budget = 360 # initialize daily minute budget
       list_for_day = Array.new
-      # logger.debug "Tasks: #{tasks.count}, Budget: #{budget}, List: #{list_for_day.count}"
+
+      # Run the loop that checks if a day still has budget
       tasks.each do |task|
-        if budget > 0 
+        if budget > 0
           # group tasks into days
           list_for_day << task
 
           # update our counters
           budget -= task.duration
           tasks = tasks.reject {|member| member == task}
-
-          # logger.debug "Tasks: #{tasks.count}, Budget: #{budget}, List: #{list_for_day.count}"
         end
       end
 
+      # Add each day to tasks_by_day array
       tasks_by_day << list_for_day
-      # logger.debug tasks_by_day.to_yaml
 
+      # logger.debug tasks_by_day.to_yaml
     end
+
     tasks_by_day
   end
 
@@ -45,8 +47,8 @@ class Task < ActiveRecord::Base
       self.duration = 30
     elsif self.duration <= 45
       self.duration = 45
-    elsif self.duration <= 60 
-      self.duration = 60 
+    elsif self.duration <= 60
+      self.duration = 60
    else
       self.duration = 75
     end
